86d598dff31018d6368d4a3edeb898a055bf2813
Joseph Pacitto <jpacitto@umassd.edu>
2018-10-24 00:40:35 -0400

    adsfasf

--- CIS454_Project2_Group1/project2.js
+++ CIS454_Project2_Group1/project2.js
@@ -7,7 +7,19 @@ var material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+var geometry = new THREE.BoxGeometry(1,1,1);
+var groundMirror = new THREE.Reflector( geometry, {
+	clipBias: 0.003,
+	textureWidth: WIDTH * window.devicePixelRatio,
+	textureHeight: HEIGHT * window.devicePixelRatio,
+	color: 0x777777,
+	recursion: 1
+} );
+groundMirror.position.y = 0.5;
+groundMirror.rotateX( - Math.PI / 2 );
+scene.add( groundMirror );
+
+camera.position.z = 4;
 
 var animate = function () {
 	requestAnimationFrame( animate );
--- CIS454_Project2_Group1/scene.js
+++ CIS454_Project2_Group1/scene.js
@@ -1,6 +1,6 @@
 //create scene and camera
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.5, 2000 );
 
 //create renderer
 var renderer = new THREE.WebGLRenderer();
